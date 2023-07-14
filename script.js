b2a11850cab8c141b6c75b4f978233d61e261c4b
Aung Pyay Thar <132377611+aungpyaythar1998@users.noreply.github.com>
2023-07-14 19:22:51 +0630

    Add password form border hint for password matches

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+function showPwdMatch() {
+    pwdForm.classList.add("match-form");
+    confirmPwdForm.classList.add("match-form");
+
+    pwdForm.classList.remove("mis-match-form");
+    confirmPwdForm.classList.remove("mis-match-form");
+
+    pwdMatchHint.classList.add("password-match");
+    pwdMatchHint.classList.remove("password-no-match");
+}
+
+function showPwdMismatch() {
+    pwdForm.classList.add("mis-match-form");
+    confirmPwdForm.classList.add("mis-match-form");
+
+    pwdForm.classList.remove("match-form");
+    confirmPwdForm.classList.remove("match-form");
+
+    pwdMatchHint.classList.add("password-no-match");
+    pwdMatchHint.classList.remove("password-match");
+}
+
+function removeAllPwdHints() {
+    pwdForm.classList.remove("match-form");
+    pwdForm.classList.remove("mis-match-form");
+
+    confirmPwdForm.classList.remove("match-form");
+    confirmPwdForm.classList.remove("mis-match-form");
+
+    pwdMatchHint.classList.remove("password-match");
+    pwdMatchHint.classList.remove("password-no-match");
+}
+
 function checkIfPwdMatches(e) {
     let pwd = pwdForm.value;
     let confirmPwd = confirmPwdForm.value;
@@ -5,17 +38,14 @@ function checkIfPwdMatches(e) {
     if (confirmPwd === "" || pwd === "") {
         // Clean up leftover added classes if one of the form
         // is empty again.
-        pwdMatchHint.classList.remove("password-no-match");
-        pwdMatchHint.classList.remove("password-match")
+        removeAllPwdHints();
     }
 
     if (confirmPwd && pwd) {
         if (confirmPwd == pwd) {
-            pwdMatchHint.classList.add("password-match");
-            pwdMatchHint.classList.remove("password-no-match");
+            showPwdMatch();
         } else {
-            pwdMatchHint.classList.add("password-no-match");
-            pwdMatchHint.classList.remove("password-match");
+            showPwdMismatch();
         }
     }
 }
